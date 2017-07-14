b99743ad1a5f189700ab9c7e12afe7796ba5696c
games647 <games647@users.noreply.github.com>
2017-07-14 17:36:28 +0200

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,45 @@
+# Feedload
+
+## Description
+
+Small and simple go application that reads an RSS feed and downloads all episodes 
+starting from the latest one. Rather than many shell or python scripts, this
+application saves the episodes according to their episode title rather than their
+file name on the server. 
+
+Furthermore, it downloads the episodes in parallel with the aim of learning this 
+programming language go.
+
+## Features
+
+* Starts 8 workers and downloads the files in parallel
+* Progress bar for each worker
+* Total progress bar
+* Native binary
+
+## Compiling
+
+1. Download project
+2. Download dependencies:
+    `go get github.com/mmcdole/gofeed`
+3. Build executable `go build feedload.go`
+
+## Running
+
+* `go run feedload.go <RSS_URL>` (Without compiling and with golang installed)
+* `./feedload <RSS_URL>` (*Nix)
+* `feedload <RSS_URL>` (Windows)
+
+## Example Output
+
+
+>Downloading all episodes from PODCAST_NAME \
+All                             0/339 [-------------------]  0s  0 % \
+Episode 205: XXXX     1.8MiB/105.1MiB [->-----------------] 1m35s 2 % \
+Episode 208: XXXX     1.6MiB/106.2MiB [->-----------------]  5s  2 % \
+Episode 209: XXXX     1.6MiB/105.3MiB [->-----------------] 45s  2 % \
+Episode 203: XXXX     1.7MiB/109.2MiB [->-----------------]  6s  2 % \
+Episode 206: XXXX     1.7MiB/111.7MiB [->-----------------]  6s  2 % \
+Episode 210: XXXX     1.7MiB/124.3MiB [>------------------]  7s  1 % \
+Episode 204: XXXX     1.3MiB/108.5MiB [>------------------]  9s  1 % \
+Episode 207: XXXX     1.2MiB/110.0MiB [>------------------] 10s  1 % \
---
+++ feedload.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	"github.com/vbauerster/mpb"
+	"github.com/vbauerster/mpb/decor"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const WORKERS = 8
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("No target rss url specified")
+	}
+
+	rssUrl := os.Args[1]
+	download(rssUrl)
+	fmt.Println("Finished")
+}
+
+func download(url string) {
+	progress := mpb.New()
+
+	//start workers
+	jobs := make(chan gofeed.Item, 2)
+	done := make(chan struct{}, 2)
+	for w := 0; w < WORKERS; w++ {
+		go worker(jobs, done, progress)
+	}
+
+	//read rss
+	feed, err := gofeed.NewParser().ParseURL(url)
+	if err != nil {
+		log.Fatal("Cannot parse feed source", err)
+	}
+
+	//count all episodes to make an progress bar
+	totalEpisodes := len(feed.Items)
+	go progressCounter(progress, totalEpisodes, done)
+
+	//submit all rss entries to the workers
+	fmt.Println("Downloading all episodes from", feed.Title)
+	for _, item := range feed.Items {
+		jobs <- *item
+	}
+
+	//Wait and clean up
+	close(jobs)
+	close(done)
+	progress.Stop()
+}
+
+func progressCounter(progress *mpb.Progress, totalEpisodes int, done <-chan struct{}) {
+	bar := createBar(progress, "All", 0, int64(totalEpisodes))
+
+	for range done {
+		bar.Incr(1)
+	}
+}
+
+func worker(jobs <-chan gofeed.Item, done chan<- struct{}, progress *mpb.Progress) {
+	for item := range jobs {
+		enclosures := item.Enclosures
+		downloadSource := enclosures[0].URL
+
+		//extract the file extension from the url
+		split := strings.Split(downloadSource, ".")
+		fileExt := split[len(split)-1]
+
+		downloadFile(item.Title, fileExt, downloadSource, progress)
+		done <- struct{}{}
+	}
+}
+
+func downloadFile(title string, ext string, url string, progress *mpb.Progress) {
+	//create output file
+	out, err := os.Create(title + "." + ext)
+	defer out.Close()
+	if err != nil {
+		//cancel processing of this file
+		log.Println("Failed to create file", err)
+		return
+	}
+
+	//connect with http
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
+	//add progress with the maximum file size
+	bar := createBar(progress, title, decor.Unit_KiB, resp.ContentLength)
+	defer progress.RemoveBar(bar)
+
+	//write contents to disk
+	_, err = io.Copy(out, bar.ProxyReader(resp.Body))
+	if err != nil {
+		log.Println("Failed to download file", err)
+	}
+}
+
+func createBar(progress *mpb.Progress, name string, counterUnit decor.Units, total int64) *mpb.Bar {
+	return progress.AddBar(total,
+		mpb.PrependDecorators(
+			decor.Name(name, 0, decor.DwidthSync),
+			decor.Counters("%3s/%3s", counterUnit, 18, 0),
+		),
+		mpb.AppendDecorators(
+			decor.ETA(3, 0),
+			decor.Percentage(5, 0),
+		))
+}
